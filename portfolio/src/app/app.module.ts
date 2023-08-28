674bb2fd56598b9ebb070348e69698d869160192
100sq <67575246+100sq@users.noreply.github.com>
2023-08-28 08:43:16 +0300

    Staring work on cards.

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EventComponent } from './event/event.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CardComponent } from './models/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventComponent,
+    CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

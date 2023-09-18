4a6574ffb6d3cd69b325eb3219d07c68b1bc1736
IvoRum <ivailo.rumenov@outlook.com>
2023-09-18 22:09:34 +0300

    da

--- portfolio/src/app/models/material/matarial/matarial.module.ts
+++ portfolio/src/app/models/material/matarial/matarial.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-
+import { MatGridListModule } from '@angular/material/grid-list';
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatCardModule, MatIconModule],
-  exports: [MatCardModule, MatIconModule],
+  imports: [CommonModule, MatCardModule, MatIconModule, MatGridListModule],
+  exports: [MatCardModule, MatIconModule, MatGridListModule],
 })
 export class MatarialModule {}

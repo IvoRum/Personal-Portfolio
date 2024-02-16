a0bf812e7e8bbf363af1d9e67f74f65afe5ad6ac
irumenov2 <ivaylo.rumenov2@dxc.com>
2024-02-16 11:28:06 +0200

    Created and used new model for reusable components like header

--- src/app/models/landing/landing.module.ts
+++ src/app/models/landing/landing.module.ts
@@ -4,9 +4,10 @@ import { MainComponent } from './main/main.component';
 import { CardComponent } from './card/card.component';
 import { MatarialModule } from '../material/matarial/matarial.module';
 import { LandingRoutingModule } from './landing-routing.module';
+import { ReusableModule } from '../reusable/reusable.module';
 
 @NgModule({
   declarations: [MainComponent, CardComponent],
-  imports: [CommonModule, MatarialModule, LandingRoutingModule],
+  imports: [CommonModule, MatarialModule, LandingRoutingModule, ReusableModule],
 })
 export class LandingModule {}
